107d2780b6d8d7594d2da67d54172dcaf6465904
KaMiL374346363 <kamil.qehreman002@gmail.com>
2024-03-17 23:54:20 +0400

    Two task

---
+++ Home work C#/Program.cs
@@ -0,0 +1,39 @@
+﻿
+//2)
+
+//int[] numsOne = { 2, 8, 1, 4, 9, 7 };
+//int[] numsTwo = { 2, 1, 6, 8, 5, 9 };
+
+//for (int i = 0; i < numsOne.Length; i++)
+//{
+//    for (int j = 0; j < numsTwo.Length; j++)
+//    {
+//        if (numsOne[i] == numsTwo[j])
+//        {
+//            Console.WriteLine(numsOne[i]);
+//        }
+//    }
+//}
+
+
+
+
+
+//3)
+
+
+//int[] arr = { 5, 1, 3, 2 };
+//int temp = 0;
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    for (int j = i + 1; j < arr.Length; j++)
+//    {
+//        if (arr[j] < arr[i])
+//        {
+//            temp = arr[j];
+//            arr[j] = arr[i];
+//            arr[i] = temp;
+//        }
+//    }
+//    Console.WriteLine(arr[i]);
+}
